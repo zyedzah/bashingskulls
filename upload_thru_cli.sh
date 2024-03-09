235b3248f667945d00a744f5620f9dac228e05e4
zyedzah <syedshah1337+github@gmail.com>
2024-03-08 20:12:12 -0500

    upload_thru_cli.sh

---
+++ upload_thru_cli.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Purpose of this file is to upload to github with CLI.
+
+# Prompt user for name.
+echo -n "Enter you name: "
+read name
+
+# Greeting
+echo "Hello, $name! Hopefully this file got uploaded to github."
+
+#Goodbye
